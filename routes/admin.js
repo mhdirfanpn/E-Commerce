92e0410bdf5f3d6d36c593b5de8a892b3730a72f
Muhammed Irfan <mhdirfanpn@gmail.com>
2022-12-29 12:19:30 +0530

    tvcr commit

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,13 @@ var db = require("../Model/connection");
 
 //verify login
 
-const verifiLogin = (req, res, next) => {
-  if (req.session.admin) {
-    next();
-  } else {
-    res.redirect("/admin/logout");
-  }
-};
+// const  = (req, res, next) => {
+//   if (req.session.admin) {
+//     next();
+//   } else {
+//     res.redirect("/admin/logout");
+//   }
+// };
 
 router.get("/", adminController.loginPage);
 
@@ -24,97 +24,95 @@ router.get("/logout", adminController.logout);
 
 router.get("/users-list", adminController.userList);
 
-router.get("/block-user/:id", verifiLogin, adminController.blockUser);
+router.get("/block-user/:id", adminController.blockUser);
 
-router.get("/unblock-user/:id", verifiLogin, adminController.unBlockUser);
+router.get("/unblock-user/:id", adminController.unBlockUser);
 
 //product
 
-router.get("/products-list", verifiLogin, adminController.productList);
+router.get("/products-list", adminController.productList);
 
-router.get("/add-product", verifiLogin, adminController.productForm);
+router.get("/add-product", adminController.productForm);
 
-router.post("/add-product", verifiLogin, adminController.addProduct);
+router.post("/add-product", adminController.addProduct);
 
-router.get("/delete-product/:id", verifiLogin, adminController.deleteProduct);
+router.get("/delete-product/:id", adminController.deleteProduct);
 
-router.get("/edit-product/:id", verifiLogin, adminController.getProduct);
+router.get("/edit-product/:id", adminController.getProduct);
 
-router.post("/edit-product/:id", verifiLogin, adminController.updateProduct);
+router.post("/edit-product/:id", adminController.updateProduct);
 
 //category
 
-router.get("/product-category", verifiLogin, adminController.categories);
+router.get("/product-category", adminController.categories);
 
-router.post("/add-category", verifiLogin, adminController.addCategory);
+router.post("/add-category", adminController.addCategory);
 
-router.get("/delete-category/:id", verifiLogin, adminController.deleteCategory);
+router.get("/delete-category/:id", adminController.deleteCategory);
 
-router.get("/edit-category/:id", verifiLogin, adminController.getCategory);
+router.get("/edit-category/:id", adminController.getCategory);
 
-router.post("/edit-category/:id", verifiLogin, adminController.updateCategory);
+router.post("/edit-category/:id", adminController.updateCategory);
 
 //orders
 
-router.get("/order-list", verifiLogin, adminController.userOrders);
+router.get("/order-list", adminController.userOrders);
 
-router.get("/order-details/:id", verifiLogin, adminController.getOrderProducts);
+router.get("/order-details/:id", adminController.getOrderProducts);
 
-router.post("/change-status", verifiLogin, adminController.changeStatus);
+router.post("/change-status", adminController.changeStatus);
 
-router.post("/approve-refund", verifiLogin, adminController.approveRefund);
+router.post("/approve-refund", adminController.approveRefund);
 
 //charts
 
-router.get("/test", verifiLogin, adminController.dailyRevenue);
+router.get("/test", adminController.dailyRevenue);
 
-router.get("/test1", verifiLogin, adminController.getVisitorGraph);
+router.get("/test1", adminController.getVisitorGraph);
 
-router.get("/test2", verifiLogin, adminController.orderCount);
+router.get("/test2", adminController.orderCount);
 
-router.get("/test3", verifiLogin, adminController.paymentMethod);
+router.get("/test3", adminController.paymentMethod);
 
 //banner
 
-router.get("/banner", verifiLogin, adminController.banner);
+router.get("/banner", adminController.banner);
 
-router.get("/add-banner", verifiLogin, adminController.addBanner);
+router.get("/add-banner", adminController.addBanner);
 
-router.post("/add-banner", verifiLogin, adminController.updateBanner);
+router.post("/add-banner", adminController.updateBanner);
 
-router.post("/edit-banner/:id", verifiLogin, adminController.editBanner);
+router.post("/edit-banner/:id", adminController.editBanner);
 
-router.get("/getBanner/:id", verifiLogin, adminController.getBanner);
+router.get("/getBanner/:id", adminController.getBanner);
 
-router.get("/deleteBanner/:id", verifiLogin, adminController.deleteBanner);
+router.get("/deleteBanner/:id", adminController.deleteBanner);
 
 //coupan
 
-router.get("/coupan", verifiLogin, adminController.getCoupan);
+router.get("/coupan", adminController.getCoupan);
 
-router.get("/coupan-form", verifiLogin, adminController.coupanForm);
+router.get("/coupan-form", adminController.coupanForm);
 
-router.post("/add-coupan", verifiLogin, adminController.addCoupan);
+router.post("/add-coupan", adminController.addCoupan);
 
 //offer
 
-router.get("/viewOffer", verifiLogin, adminController.viewOffer);
+router.get("/viewOffer", adminController.viewOffer);
 
 router.post(
   "/admin-addCategoryOffer",
-  verifiLogin,
   adminController.addCategoryOffer
 );
 
 router.get(
   "/delete-offer/:id",
-  verifiLogin,
   adminController.removeCategoryOffer
 );
 
 //report
 
-router.get("/get-Report", verifiLogin, adminController.salesReport);
+router.get("/get-Report", adminController.salesReport);
 
 router.post("/sales-rep-mon", adminController.monthlySalesReport);
 
