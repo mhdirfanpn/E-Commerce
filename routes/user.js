2d08f30bce7badad4773eefe92a9b0024381b644
Muhammed Irfan <mhdirfanpn@gmail.com>
2022-12-29 12:20:51 +0530

    rhdrt commit

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const swal = require("sweetalert");
 const verifyLogin = async(req, res, next) => {
   let user=req.session.user
   userHelpers.findUser(user).then((userData)=>{
-    if (user && !userData?.isBlocked) {
+    if (user) {
       next();
     } else {
 
