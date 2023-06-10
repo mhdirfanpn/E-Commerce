34d6a6a79d04e0ddd471c07bab2af39f52d7826b
mhdirfanikru <irfanikru123404@gmail.com>
2023-06-10 14:44:23 +0530

    image update

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function (req, res, next) {
 app.use("/", userRouter);
 app.use("/admin", adminRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 
 app.use(function (req, res, next) {
   next(createError(404));
 });
